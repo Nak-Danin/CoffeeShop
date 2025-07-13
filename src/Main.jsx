c54f46eb343e76e4d74c7e1ea338350e3f728d19
Danin <ninsweetie859@gmail.com>
2025-07-13 20:46:30 +0700

    Changed Main.jsx to main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import '@fontsource/roboto-condensed/400.css'; // Regular (Normal)
 import '@fontsource/roboto-condensed/700.css'; // Bold
 
