2af039a8011a7e47a625bb87b8b9ee7509240ada
Danin <ninsweetie859@gmail.com>
2025-07-15 15:51:21 +0700

    Added ScrollToTop

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,8 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import { Navbar, Footer } from "../main/index";
+import ScrollToTop from "../utils/ScrollToTop";
 const RootLayout = () => {
   return (
     <div className="w-full h-fit">
+      <ScrollToTop/>
       <Navbar />
       <Outlet />
       <Footer />
